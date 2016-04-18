f0fa936bba4b748ecbd4df0535931289e200238a
Jona <jmeroy03@gmail.com>
2016-04-18 02:46:33 -0700

    product page

--- classes/review.php
+++ classes/review.php
@@ -16,6 +16,33 @@ class Review{
 	}
 
 	public function addReview($rating,$titleReview,$titleBody){
+		$sql = "INSERT INTO movies(filmName,
+            filmDescription,
+            filmImage,
+            filmPrice,
+            filmReview) VALUES (
+            :filmName, 
+            :filmDescription, 
+            :filmImage, 
+            :filmPrice, 
+            :filmReview)";
+                                          
+			$stmt = $pdo->prepare($sql);
+                                              
+			$stmt->bindParam(':filmName', $_POST['filmName'], PDO::PARAM_STR);       
+			$stmt->bindParam(':filmDescription', $_POST['filmDescription'], PDO::PARAM_STR); 
+			$stmt->bindParam(':filmImage', $_POST['filmImage'], PDO::PARAM_STR);
+		// use PARAM_STR although a number  
+			$stmt->bindParam(':filmPrice', $_POST['filmPrice'], PDO::PARAM_STR); 
+			$stmt->bindParam(':filmReview', $_POST['filmReview'], PDO::PARAM_STR);   
+                                      
+			$stmt->execute(); 
+
+
+
+
+
+
 		$insert = "INSERT INTO reviews (product_id, 
 										customer_id, 
 										datetime_posted,
@@ -35,6 +62,10 @@ class Review{
 	}
 
 	public function updateReview(){
+
+
+
+
 
 	}
 
---
+++ product_page.php
@@ -0,0 +1,9 @@
+<?php
+	include 'header.php';
+?>
+
+<ul class="nav nav-tabs">
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+</ul>
